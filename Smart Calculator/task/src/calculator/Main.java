a8b92b67cf7d047d9b45bdb692c273e35e720851
simplebunsen <really@dontdoxme.com>
2020-05-09 13:24:16 +0200

    Progress on variables.

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -1,15 +1,14 @@
 package calculator;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+
+    public static Map<String, Long> variableMap = new HashMap<>();
+    public static String regexOperatorDigit = "[-+ ]*[0-9]*";
+    public static Pattern pattern = Pattern.compile(regexOperatorDigit);
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,6 +17,8 @@ public class Main {
             String inputString = scanner.nextLine();
 
             if (inputString.isEmpty()) continue;
+
+            /* COMMANDS */
             if (inputString.matches("/.*")) {
                 if (inputString.equals("/exit")) {
                     break;
@@ -29,27 +30,40 @@ public class Main {
                     continue;
                 }
             }
-            if(!inputString.matches("([-+ ]*[0-9])*")){
-                System.out.println("Invalid expression");
+
+            /* VARIABLE ASSIGNMENTS */
+            if (inputString.matches(".* *(= *.*)")){
+                saveVariable(inputString);
                 continue;
             }
 
-            String regexOperatorDigit = "[-+ ]*[0-9]*";
-            Pattern pattern = Pattern.compile(regexOperatorDigit);
+            /* VARIABLE PRINTING TODO: might be useless due to normal calculator logic getting the var automatically*/
+            if (inputString.matches("[a-zA-Z]*") && variableMap.containsKey(inputString)){
+                System.out.println(variableMap.get(inputString));
+                continue;
+            }
+
+            /* CATCH ALL FOR INVALID INPUT */
+            if(!inputString.matches("([-+ ]*[0-9])*")){
+                System.out.println("Invalid expression: Catch All");
+                continue;
+            }
+
+
             Matcher m = pattern.matcher(inputString);
             List<String> list = getElementsArrayList(m);
             if (list.contains("?")){
-                System.out.println("Invalid expression");
+                System.out.println("Invalid expression: ? found in list");
                 continue;
             }
 
-            //System.out.println("list of elements:" + list);
+            System.out.println("list of elements:" + list);
 
             if(list.get(0).matches("[+-]")){
                 list.add(0, "0");
             }
             int result = Integer.parseInt(list.get(0));
-            for (int i = 1; i < list.size(); i= i+2) {
+            for (int i = 1; i < list.size(); i = i+2) {
                 String operator = list.get(i);
                 int n = Integer.parseInt(list.get(i+1));
                 switch (operator) {
@@ -72,6 +86,40 @@ public class Main {
             System.out.println(result);
         }
         System.out.println("Bye!");
+    }
+
+    private static void saveVariable(String inputString) {
+        if (inputString.matches("=")) {
+            System.out.println("Invalid assignment: input is nothing but an equals sign, you kek");
+            return;
+        }
+        String[] inputElements = inputString.split("=");
+
+        if (varAssignmentHasMistake(inputElements)) return;
+
+        long number = Long.parseLong(condenseSigns(inputElements[1].trim()));
+        System.out.printf("we put %s as %d\n", inputElements[0].trim(), number);
+        variableMap.put(inputElements[0].trim(), number);
+    }
+
+    private static boolean varAssignmentHasMistake(String[] inputElements) {
+        if (inputElements.length < 2) {
+            System.out.println("Invalid assignment: one side doesn't have input!");
+            return true;
+        }
+        if (inputElements.length > 2) {
+            System.out.println("Invalid assignment: more than 1 equals sign!");
+            return true;
+        }
+        if (!inputElements[0].trim().matches("[a-zA-Z]+")){
+            System.out.println("Invalid identifier: You can only use latin letters as variable name!");
+            return true;
+        }
+        if (!inputElements[1].trim().matches("[-+ ]*[0-9]+")){ //TODO: remember fractions later!
+            System.out.println("Invalid assignment: Value has symbols other than numbers and + -");
+            return true;
+        }
+        return false;
     }
 
     private static List<String> getElementsArrayList(Matcher m) {
@@ -103,13 +151,18 @@ public class Main {
             operator = substring.charAt(0);
             substring = substring.substring(1);
         }
-        String cleanNumber = substring;
-        //while current substring is bigger than the digits of the substring and 1 operator
-        while (cleanNumber.length() > cleanNumber.replaceAll("\\D", "").length() + 1) {
-            cleanNumber = cleanNumber.replaceAll("\\+-|-\\+", "-")
+        String cleanNumber = condenseSigns(substring);
+        return new String[] {String.valueOf(operator), cleanNumber};
+    }
+
+    private static String condenseSigns(String input) {
+        //while current substring is bigger than the digits of the substring and 1 sign
+        while (input.length() > input.replaceAll("\\D", "").length() + 1) {
+            input = input.replaceAll("\\+-|-\\+", "-")
                                     .replaceAll("\\++", "")
                                     .replaceAll("--", "+");
         }
-        return new String[] {String.valueOf(operator), cleanNumber};
+        //System.out.println("output is now " + input);
+        return input;
     }
 }
